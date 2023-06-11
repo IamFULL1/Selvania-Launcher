0e6e778d95901b929914714e2b06549d85dac145
IamFULL1 <103425249+IamFULL1@users.noreply.github.com>
2023-06-11 01:45:34 -0300

    1.3.3

---
+++ src/renderer/src/components/shared/navigation/parts/avatar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import * as Tabs from '@radix-ui/react-tabs'
+
+import { useAuthStore } from '../../../../stores'
+
+import { ColorExtractor } from 'react-color-extractor'
+
+const Avatar = function () {
+  const { user } = useAuthStore()
+  const [bgColor, updateBgColor] = React.useState('rgb(255 255 255 / 0.05)')
+
+  const imageUrl = `https://minotar.net/armor/bust/${user ? user.name : 'undefined'}/80.png`
+
+  const handleExtractionComplete = React.useCallback((colorPalette) => {
+    const leastProminentColor = colorPalette[colorPalette.length - 1]
+    updateBgColor(leastProminentColor)
+  }, [])
+
+  return (
+    <Tabs.Trigger value="settings" asChild>
+      <button
+        className="w-12 h-12 flex center shrink-0 grow-0 rounded-b-2xl rounded-t-md bg-white/5 overflow-hidden cursor-pointer group"
+        style={{ background: bgColor }}
+      >
+        <ColorExtractor getColors={handleExtractionComplete}>
+          <img
+            src={imageUrl}
+            className="w-full h-full object-cover object-center relative transition-all duration-200 ease-in-out"
+            alt={`Avatar do ${user?.name}`}
+            draggable={false}
+          />
+        </ColorExtractor>
+      </button>
+    </Tabs.Trigger>
+  )
+}
+
+export default Avatar
